e71936cd693812c110f095ddb90eb8685381b886
Kohei Yamamoto <kymmt90@gmail.com>
2020-02-24 18:02:06 +0900

    Read from stdin and count lines

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
-import Lib
+import Data.Maybe (fromJust)
+import Lib (countLines)
+
+execute :: String -> Maybe Int
+execute text = Just (countLines text)
 
 main :: IO ()
-main = return ()
+main = do
+  text <- getContents
+  return (show . fromJust . execute $ text) >>= putStrLn
