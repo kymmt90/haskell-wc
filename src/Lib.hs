9c4b6f90ee5bfec5011c4c13bab3f7e0dbc6eea8
Kohei Yamamoto <kymmt90@gmail.com>
2020-02-24 17:19:10 +0900

    Add countLines test

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,6 @@
 module Lib
-    ( someFunc
+    ( countLines
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+countLines :: String -> Int
+countLines = length . lines
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,24 @@
+import Lib (countLines)
 import Test.HUnit
 
 main :: IO ()
 main = do
-  _ <- runTestTT $ TestList [sampleTest]
+  _ <- runTestTT $ TestList
+    [
+      sampleTest
+    , countLinesTest
+    ]
   return ()
 
 sampleTest :: Test
 sampleTest = TestList
   [ "Test 1" ~: (1 + 1 :: Int) ~?= 2
+  ]
+
+countLinesTest :: Test
+countLinesTest = TestList
+  [ "counts lines of a string" ~:
+    countLines "ab\ncde\nf\n" ~?= 3
+  , "counts an empty line" ~:
+    countLines "" ~?= 0
   ]
