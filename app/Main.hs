0713f596c2c4e2754e5f557a746fe9d0ba874081
Kohei Yamamoto <kymmt90@gmail.com>
2020-03-28 13:30:08 +0900

    Move logic to lib

--- app/Main.hs
+++ app/Main.hs
@@ -2,33 +2,8 @@
 
 module Main where
 
+import Lib
 import System.Environment (getArgs)
-import Text.Printf (printf)
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as TE
-import qualified Data.Text.IO as TIO
-import qualified Data.ByteString as BS
-
-data Result = Result {
-  numberOfLines :: Int
-  , numberOfWords :: Int
-  , numberOfBytes :: Int
-  }
-
-instance Show Result where
-  show r = printf "%7d%8d%8d" (numberOfLines r) (numberOfWords r) (numberOfBytes r)
-
-readStdin :: IO T.Text
-readStdin = TIO.getContents
-
-countLines :: T.Text -> Int
-countLines = length . T.lines
-
-countWords :: T.Text -> Int
-countWords = length . T.words
-
-countBytes :: T.Text -> Int
-countBytes = BS.length . TE.encodeUtf8
 
 main :: IO ()
 main = do
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,1 +1,28 @@
 module Lib where
+
+import Text.Printf (printf)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text.IO as TIO
+import qualified Data.ByteString as BS
+
+data Result = Result {
+  numberOfLines :: Int
+  , numberOfWords :: Int
+  , numberOfBytes :: Int
+  }
+
+instance Show Result where
+  show r = printf "%7d%8d%8d" (numberOfLines r) (numberOfWords r) (numberOfBytes r)
+
+readStdin :: IO T.Text
+readStdin = TIO.getContents
+
+countLines :: T.Text -> Int
+countLines = length . T.lines
+
+countWords :: T.Text -> Int
+countWords = length . T.words
+
+countBytes :: T.Text -> Int
+countBytes = BS.length . TE.encodeUtf8
