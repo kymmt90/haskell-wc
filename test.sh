5c6c35851011077859f290b6c9a6c46c898576c8
Kohei Yamamoto <kymmt90@gmail.com>
2020-03-28 17:42:35 +0900

    fix

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ expected=`cat lorem_ipsum.txt | wc`
 echo "$expected"
 actual=`cat lorem_ipsum.txt | stack exec wc-exe 2>/dev/null`
 echo "$actual"
-if [ "$expected" = "$actual" ]; then
-    echo 'ok'
-else
-    echo 'ng'
-fi
+
+expected=`wc lorem_ipsum.txt`
+echo "$expected"
+actual=`stack exec wc-exe lorem_ipsum.txt 2>/dev/null`
+echo "$actual"
